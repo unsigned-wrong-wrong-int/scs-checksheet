bd914873b5170c3e2e63c02f5463e16d22277a62
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2023-01-14 17:08:43 +0900

    Reduce JSON file size

--- data/tojson.py
+++ data/tojson.py
@@ -18,7 +18,9 @@ patterns = [
     (r'GA14121', r'GA14111'),
     (r'HC30241', r'HC30141'),
     (r'HC21371', r'HC36191'),
-    (r'HC21271', r'HC21071')
+    (r'HC21271', r'HC21071'),
+    (r'21...[2468]', r'19'),
+    (r'21...[3579]', r'20')
 ]
 
 def normalize(id_):
@@ -29,36 +31,46 @@ def normalize(id_):
             break
 
 subjects = {
-    '0': ['全ての科目', 0.0, 0],
-    '1': ['全ての科目 (初修外国語・体育を除く)', 0.0, 1],
-    '2': ['専門基礎科目・専門科目', 0.0, 2],
-    '3': ['専門導入科目', 0.0, 4],
-    '4': ['専門導入科目・看護学類が指定する科目', 0.0, 8],
-    '5': ['医学類開設科目', 0.0, 16],
-    '6': ['体育 (春・秋)', 1.0, 32],
-    '7': ['英語 (春)', 2.0, 128],
-    '8': ['英語 (春・秋)', 4.0, 128 | 256],
-    '9': ['情報', 4.0, 512],
-    '10': ['ファーストイヤーセミナー', 1.0, 1024],
-    '11': ['学問への誘い', 1.0, 2048]
+    '0': ['全ての科目', 0, 1],
+    '1': ['全ての科目(初修外国語・体育を除く)', 0, 2],
+    '2': ['専門基礎科目・専門科目', 0, 4],
+    '3': ['専門導入科目', 0, 8],
+    '4': ['専門導入科目・看護学類が指定する科目', 0, 16],
+    '5': ['医学類開設科目', 0, 32],
+    '6': ['体育(春・秋)', 1, 128],
+    '7': ['英語(春)', 2, 256],
+    '8': ['英語(春・秋)', 4, 512],
+    '9': ['情報', 4, 1024],
+    '10': ['ファーストイヤーセミナー', 1, 0],
+    '11': ['学問への誘い', 1, 0],
+    '12': ['情報リテラシー(講義)', 1, 1024],
+    '13': ['情報リテラシー(演習)', 1, 1024],
+    '14': ['データサイエンス', 2, 1024],
+    '15': ['English Reading Skills I', 1, 256 | 512],
+    '16': ['English Presentation Skills I', 1, 256 | 512],
+    '17': ['English Reading Skills II', 1, 256],
+    '18': ['English Presentation Skills II', 1, 256],
+    '19': ['基礎体育(春)', 0.5, 128],
+    '20': ['基礎体育(秋)', 0.5, 128]
 }
 
 def subject(row):
     id_, name, credit, semester, _, info = row
     credit = float(credit)
-    this_year = re.search(r'秋(?:A?B?C|学期)|春季休業中|通年', semester) is None
-    flags = 0 if this_year else 32
-    if re.match(r'21.*', id_) is None:
-        flags |= 1
+    if int(credit) == credit:
+        credit = int(credit)
+    flags = 1 | 2
     if re.match(r'[A-Y].*', id_) is not None:
-        flags |= 2
+        flags |= 4
         if (re.search(r'専門導入科目', info) or re.match(r'FCA1961|FE11431', id_)) is not None:
-            flags |= 4 | 8
+            flags |= 8 | 16
         if re.match(r'HB(?:211[046-9]|212[03]|3113)1', id_) is not None:
-            flags |= 8
-        if re.match(r'AB6.*|C[CE].*|AC(?:50H[1267]|6[34]E4|63G[01]|64A[2-5]|65E[2-5]|65A[1-467])1', id_) is not None:
             flags |= 16
-    elif re.match(r'3[2-7][HJKL].*', id_) is not None:
+        if re.match(r'AB6.*|C[CE].*|AC(?:50H[1267]|6[34]E4|63G[01]|64A[2-5]|65E[2-5]|65A[1-467])1', id_) is not None:
+            flags |= 32
+    elif re.match(r'(?:2|3[2-7]).*', id_) is not None:
+        flags = 1
+    if re.search(r'秋(?:A?B?C|学期)|春季休業中|通年', semester) is not None:
         flags |= 64
     return (id_, [name, credit, flags, *normalize(id_)])
 
@@ -69,18 +81,7 @@ try:
 finally:
     wb.close()
 
-common = [
-    ['ファーストイヤーセミナー', 1.0, 1024],
-    ['学問への誘い', 1.0, 2048],
-    ['情報リテラシー(講義)', 1.0, 512],
-    ['情報リテラシー(演習)', 1.0, 512],
-    ['データサイエンス', 2.0, 512],
-    ['English Reading Skills I', 1.0, 128],
-    ['English Presentation Skills I', 1.0, 128],
-    ['English Reading Skills II', 1.0, 256],
-    ['English Presentation Skills II', 1.0, 256],
-    ['基礎体育(春・秋)', 1.0, 64]
-]
+common = list(range(10, 21))
 
 with open(path.join(dir_, 'info.json'), encoding='utf-8') as f:
     data = json.load(f)
@@ -88,7 +89,7 @@ with open(path.join(dir_, 'info.json'), encoding='utf-8') as f:
 with open(path.join(dir_, 'table.json'), encoding='utf-8') as f:
     table = json.load(f)
 
-data.update(common=common, subjects=subjects, table=table)
+data.update(subjects=subjects, common=common, table=table)
 
 with open(path.join(dir_, 'data.json'), 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, separators=(',', ':'))
