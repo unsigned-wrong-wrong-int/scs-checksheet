b257d52d706815d1951041969fd6c869e211aed6
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2023-01-21 16:59:35 +0900

    Add subject.js

---
+++ scripts/subject.js
@@ -0,0 +1,71 @@
+export
+const SUBJ_ALL = 1,
+      SUBJ_HUMAN = 2,
+      SUBJ_ESYS = 4,
+      SUBJ_INTRO = 8,
+      SUBJ_NURSE = 16,
+      SUBJ_MED = 32,
+      SUBJ_FALL_C = 64,
+      SUBJ_PE_SPR_FALL = 128,
+      SUBJ_ENG_SPR = 256,
+      SUBJ_ENG_SPR_FALL = 512,
+      SUBJ_INFO = 1024;
+
+export
+const subject = (id, [name, credit, flags, id_n = id]) =>
+   ({id, name, credit, flags, id_n});
+
+export
+const STATE_FAIL = 0,
+      STATE_CURR = 1,
+      STATE_PASS = 2;
+
+export
+const GradeList = class {
+   #list = [];
+
+   add(subject, isLastYear) {
+      const beforeFallC = isLastYear || (subject.flags & SUBJ_FALL_C) === 0;
+      const v = {subject, beforeFallC, score: null, state: STATE_CURR};
+      this.#list.splice(this.#list.findIndex(u => u.subject.id > subject.id), 0, v);
+      return v;
+   }
+
+   remove(v) {
+      this.#list.splice(this.#list.indexOf(v), 1);
+   }
+
+   getByCredit(id_n) {
+      return this.#list
+         .filter(v => v.subject.id_n === id_n && v.state !== STATE_FAIL)
+         .reduce((x, v) => x?.state <= v.state ? v : x, null);
+   }
+
+   getByScore(id_n) {
+      return this.#list
+         .filter(v => v.subject.id_n === id_n)
+         .reduce((x, v) => +x?.score <= +v.score ? v : x, null);
+   }
+
+   collect(flags) {
+      return [...this.#list
+         .filter(v => v.beforeFallC && (v.subject.flags & flags) === flags)
+         .reduce((m, v) =>
+            !m.has(v.subject.id_n) || +m.get(v.subject.id_n).score <= +v.score
+               ? m.set(v.subject.id_n, v) : m
+         , new Map())
+         .values()];
+   }
+
+   get length() {
+      return this.#list.length;
+   }
+
+   *[Symbol.iterator]() {
+      yield* this.#list;
+   }
+
+   toJSON() {
+      return this.#list;
+   }
+};
