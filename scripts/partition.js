fb724d240341e62a83a52ee1ab6ec46adec25cc0
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2023-01-27 06:45:57 +0900

    Update partition.js

--- scripts/partition.js
+++ scripts/partition.js
@@ -1,101 +1,179 @@
 import {STATE_FAIL, STATE_CURR, STATE_PASS} from "./subject";
 
-const asPair = range => typeof range === "number" ? [range, range] : range;
-
-const requirementSpan = (first, last, min = 0) =>
-   min < 0 ? {first, last, limit: -min} : min > 0 ? {first, last, min} : {first, last};
-
-const requirement = ([range, min, ...sub]) => {
-   const [first, last] = asPair(range);
-   const spans = [];
-   let i = first;
-   for (const [range, min] of sub) {
-      const [first, last] = asPair(range);
-      if (i < first) {
-         spans.push(requirementSpan(i, first - 1));
+const buildRule = data => {
+   let rule = {};
+   let i;
+   if (typeof data[0] === "number") {
+      rule.first = data[0];
+      rule.last = data[1];
+      i = 2;
+   } else {
+      rule.spans = [];
+      for (i = 0; i < data.length && typeof data[i] !== "number"; ++i) {
+         rule.spans.push(buildRule(data[i], proto));
       }
-      spans.push(requirementSpan(first, last), min);
-      i = last + 1;
    }
-   if (i < last) {
-      spans.push(requirementSpan(i, last));
+   if (i < data.length) {
+      rule.count = Math.abs(data[i]);
+      rule.saturates = data[i] < 0;
+      if (++i < data.length) {
+         rule.weight = data[i];
+      }
    }
-   return {first, last, spans, min};
+   return rule;
 };
 
-const CheckContext = class {
-   constructor(list, reqs, grade) {
-      this.list = list;
-      this.reqs = reqs;
-      this.grade = grade;
+const TestContext = class {
+   constructor() {
+      this.passed = 0;
+      this.pending = 0;
       this.state = STATE_PASS;
    }
 
-   result(state) {
-      this.state = Math.min(this.state, state);
-   }
-
-   checkSpan(span) {
-      let credit = 0, currCredit = 0;
-      for (let i = span.first; i <= span.last; ++i) {
-         for (const v of this.grade.enumWithCredit(this.list[i])) {
-            if (v.state === STATE_PASS) {
-               credit += v.subject.credit;
-            } else {
-               currCredit += v.subject.credit;
-            }
-         }
-      }
-      if (span.limit) {
-         if (credit > span.limit) {
-            credit = span.limit;
-         }
-         if (currCredit + credit > span.limit) {
-            currCredit = span.limit - credit;
-         }
-      }
-      if (span.min && credit < span.min) {
-         if (credit + currCredit < span.min) {
-            this.result(STATE_FAIL);
-         } else {
-            this.result(STATE_CURR);
-         }
+   add(v) {
+      if (v.state === STATE_PASS) {
+         this.passed += v.subject.credit;
       } else {
-         this.result(STATE_PASS);
-      }
-      return [credit, currCredit];
-   }
-
-   checkReq(req) {
-      const [credit, currCredit] = req.spans.map(s => this.checkSpan(s))
-         .reduce((a, b) => [a[0] + b[0], a[1] + b[1]]);
-      if (credit < req.min) {
-         if (credit + currCredit < req.min) {
-            this.result(STATE_FAIL);
-         } else {
-            this.result(STATE_CURR);
-         }
-      } else {
-         this.result(STATE_PASS);
+         this.pending += v.subject.credit;
       }
    }
 
-   check() {
-      this.reqs.forEach(r => this.checkReq(r));
+   limit(count, saturates) {
+      if (saturates) {
+         this.passed = Math.min(this.passed, count);
+         this.pending = Math.min(this.pending, count - this.passed);
+      }
+      if (this.passed < count) {
+         this.state = this.passed + this.pending >= count
+            ? Math.min(this.state, STATE_CURR) : STATE_FAIL;
+      }
+   }
+
+   merge(other) {
+      this.passed += other.passed;
+      this.pending += other.pending;
+      this.state = Math.min(this.state, other.state);
+   }
+
+   result() {
       return this.state;
    }
 };
 
-const Partition = class {
-   constructor({name, list, min, weight, max}) {
-      this.name = name;
-      this.list = list;
-      this.reqs = min.map(requirement);
-      // TODO: weight, max
+const test = (list, grade, rule, outer = null) => {
+   const ctx = rule.count ? new TestContext() : outer;
+   if (rule.spans) {
+      for (const span of rule.spans) {
+         test(list, grade, span, ctx);
+      }
+   } else {
+      for (const id of list.slice(rule.first, rule.last + 1)) {
+         for (const v of grade.getForTest(id)) {
+            ctx.add(v);
+         }
+      }
    }
-
-   state(grade) {
-      const ctx = new CheckContext(this.list, this.reqs, grade);
-      return ctx.check();
+   if (rule.count) {
+      ctx.limit(rule.count, rule.saturates);
+      if (outer === null) {
+         return ctx.result();
+      }
+      outer.merge(ctx);
    }
 };
+
+const CalcContext = class {
+   constructor(weight) {
+      this.data = [];
+      this.weight = weight;
+      this.half = null;
+   }
+
+   add(v, credit = v.subject.credit, weight = this.weight) {
+      if (!Number.isInteger(credit)) {
+         credit = Math.trunc(credit);
+         this.addHalf(v);
+      }
+      this.addFixed(v, credit, weight);
+   }
+
+   addHalf(v) {
+      if (this.half) {
+         const u = this.half;
+         this.half = null;
+         const pair = [u, v];
+         pair.score = u.score + v.score;
+         this.addFixed(pair, 1.0, this.weight);
+      } else {
+         this.half = v;
+      }
+   }
+
+   addFixed(v, credit, weight) {
+      const i = this.data.findIndex(x => x[2] < weight || x[2] === weight && x[0].score < v.score);
+      this.data.splice(i === -1 ? this.data.length : i, 0, [v, credit, weight]);
+   }
+
+   limit(count, rest) {
+      let credit = 0;
+      for (let i = 0; i < this.data.length; ++i) {
+         const [v, c, w] = this.data[i];
+         if (credit + c >= count) {
+            this.data[i] = [v, count - credit, w];
+            if (credit + c > count) {
+               rest?.addFixed(v, c + credit - count, rest.weight)
+            }
+            for (const [v, c, ] of this.data.splice(i + 1)) {
+               rest?.addFixed(v, c, rest.weight);
+            }
+            if (this.half) {
+               rest?.addHalf(this.half);
+               this.half = null;
+            }
+            return;
+         }
+         credit += c;
+      }
+      if (this.half) {
+         this.addFixed(this.half, 0.5, this.weight);
+         this.half = null;
+      }
+   }
+
+   merge(other) {
+      for (const [v, c, w] of other.data) {
+         this.addFixed(v, c, w);
+      }
+   }
+
+   result() {
+      const sum = this.data
+         .reduce((s, [v, c, w]) => s + Math.round(100 * w * c * v.score), 0) / 100;
+      return [sum, this.data];
+   }
+};
+
+const calc = (list, grade, rule, outer = null, unweighted = null) => {
+   const ctx = rule.count ? new CalcContext(rule.weight ?? outer.weight) : outer;
+   if (rule.spans) {
+      for (const span of rule.spans) {
+         calc(list, grade, span, ctx, unweighted ?? ctx);
+      }
+   } else {
+      for (const id of list.slice(rule.first, rule.last + 1)) {
+         for (const v of grade.getForCalc(id)) {
+            ctx.add(v);
+         }
+      }
+   }
+   if (rule.count) {
+      ctx.limit(rule.count, rule.saturates ? unweighted : outer);
+      if (outer === null) {
+         return ctx.result();
+      }
+      outer.merge(ctx);
+   }
+};
+
+const partition = ({name, list, test, calc}) =>
+   ({name, list, testRule: buildRule(test), calcRule: buildRule(calc)});
