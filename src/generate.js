da10570a82f9255dcd5d5b6519136ea754a82ee9
unsigned-wrong-wrong-int <59091082+unsigned-wrong-wrong-int@users.noreply.github.com>
2024-08-31 15:40:13 +0900

    Add buttons for popover

--- src/generate.js
+++ src/generate.js
@@ -10,27 +10,36 @@ divisions.forEach(division => {
    const {list, test, sum} = division;
    const testTbl = list.map(_ => []);
    const testRng = [test.at(0)?.[0] ?? 0, test.at(-1)?.[1] ?? -1];
+   let i = 0;
+   for (const rule of test) {
+      if (rule.length === 3) {
+         rule.push(i);
+         ++i;
+      }
+      testTbl[rule[0]].unshift(rule);
+   }
    if (testRng[0] > 0) {
       testTbl[0].unshift([0, testRng[0] - 1, false]);
    }
    if (testRng[1] < list.length - 1) {
       testTbl[testRng[1] + 1].unshift([testRng[1] + 1, list.length - 1, false]);
    }
-   for (const rule of test) {
-      testTbl[rule[0]].unshift(rule);
-   }
    testTbl.cols = Math.max(...testTbl.map(({length}) => length));
    const sumTbl = list.map(_ => []);
    const sumRng = [sum.at(0)?.[0] ?? 0, sum.at(-1)?.[1] ?? -1];
+   i = 0;
+   for (const rule of sum) {
+      if (rule.length === 3) {
+         rule.push(i);
+         ++i;
+      }
+      sumTbl[rule[0]].push(rule);
+   }
    if (sumRng[0] > 0) {
       sumTbl[0].push([0, sumRng[0] - 1, false]);
    }
    if (sumRng[1] < list.length - 1) {
       sumTbl[sumRng[1] + 1].push([sumRng[1] + 1, list.length - 1, false]);
-   }
-   for (const rule of sum) {
-      if (Array.isArray(rule[0])) console.log(rule);
-      sumTbl[rule[0]].push(rule);
    }
    sumTbl.cols = Math.max(...sumTbl.map(({length}) => length));
    Object.assign(division, {test: testTbl, sum: sumTbl});
